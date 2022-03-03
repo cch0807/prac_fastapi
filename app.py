9009de898cec7f21c7de9d47335d881c6e700888
cch0807 <https://github.com/cch0807>
2022-03-03 15:19:36 +0900

    Add: delete

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from this import d
+from typing import Optional
+from urllib import response
 import uvicorn, uuid
 
 from pydantic import BaseModel
@@ -24,8 +26,16 @@ class Item(BaseModel):
   user_id: str
   password: str
 
+class ResponseItem(Item):
+  success: bool
+
+class PatchItem(BaseModel):
+  user_id: Optional[str]
+  password: Optional[str]
+
 @app.post('/register')
 async def register_item(item:Item):
+  global dicted_item
   dicted_item = dict(item)
   dicted_item['success'] = True
 
@@ -47,6 +57,11 @@ async def update_item_sub(item: Item):
   dicted_item['success'] = True
 
   return JSONResponse(dicted_item)
+
+@app.delete('/delete')
+async def delete_item():
+  dicted_item = None
+  return Response(status_code=HTTP_204_NO_CONTENT)
 
 if __name__ == '__main__':
   uvicorn.run(app,host='0.0.0.0',port=8000)
